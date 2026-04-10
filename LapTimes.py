868513c47939a3a57e6ed2e7d97fa3aa34dbe112
Tracing Insights <tracing.insights@gmail.com>
2026-04-10 16:02:39 +0530

    Update LapTimes.py

--- LapTimes.py
+++ LapTimes.py
@@ -34,14 +34,14 @@ import requests
 DEFAULT_YEAR = 2019
 # Keep exactly one uncommented event in this list.
 TARGET_EVENT_NAMES_LIST = [
-    'Abu Dhabi Grand Prix',
-    'Australian Grand Prix', 
-    'Austrian Grand Prix', 
-    'Azerbaijan Grand Prix',
-    'Bahrain Grand Prix',
-    'Belgian Grand Prix',
-    'Brazilian Grand Prix', 
-    'British Grand Prix',
+    # 'Abu Dhabi Grand Prix',
+    # 'Australian Grand Prix', 
+    # 'Austrian Grand Prix', 
+    # 'Azerbaijan Grand Prix',
+    # 'Bahrain Grand Prix',
+    # 'Belgian Grand Prix',
+    # 'Brazilian Grand Prix', 
+    # 'British Grand Prix',
     'Canadian Grand Prix',
     'Chinese Grand Prix', 
     'French Grand Prix', 
